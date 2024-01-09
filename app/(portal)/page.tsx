2c9f71a794074767e27242450b03dad5796b0c40
Eshita Jain <eshitajain555@gmail.com>
2024-01-10 00:19:03 +0530

    feat: unstyled content added

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -33,25 +33,26 @@ export default function Page() {
         }
     }
     return (
+        // new changed
         <Grid
             container
             spacing={0}
             direction="column"
-            alignItems="center"
-            justifyContent="center"
+            alignItems= "center"
+            // justifyContent="center"
             sx={{minHeight: `calc(100vh - 20px - 33px)`}}
         >
             <Grid item xs={3}>
                 <Stack direction={{xs: "column", sm: "row-reverse"}} spacing={2}>
                     <Box display="flex" sx={{justifyContent: "center"}}>
-                        <Image src='/AcmLogo.png' width={150} height={150} alt="logo" />
+                        <Image src='/AcmLogo.png' width={50} height={50} alt="logo" />
                     </Box>
                     <Box>
                         <Grid
                             display="flex"
                             justifyContent={{xs: "center", sm: "flex-start"}}
                         >
-                            <Typography variant="h3">Association Of Computing Machinery</Typography>
+                            <Typography variant="h4">Association Of Computing Machinery</Typography>
                         </Grid>
                         <Grid
                             display="flex"
@@ -90,6 +91,56 @@ export default function Page() {
                         </Grid>
                     </Box>
                 </Stack>
+            </Grid>
+            <Grid >
+                <Typography>
+                ACM Enrollment Website - User Guide
+                </Typography>
+                <Typography>
+
+👋 Welcome to the ACM Enrollment Website, designed with a familiar interface similar to VSCode. Let's get you started with some simple navigation tips.
+</Typography>
+<Typography>
+📂 File Navigation - MD Files</Typography><Typography> <br/>
+Find the file explorer panel on the left. Here, you&apos;ll find two dropdowns labeled “domains” and “extras”.
+</Typography><Typography>Clicking on the "domains" dropdown allows you to access files containing information about ACM's different domains, while "extras" offers events, FAQs, and organizational details.
+</Typography><Typography>
+💻 Integrated Terminal - Quick Access</Typography><Typography>
+The Terminal Button can be spotted at the bottom left. Click it, and a command-line interface will pop up.
+</Typography><Typography>
+Type in &apos;help&apos; to see the list of available commands. These are the commands that you will use to operate the terminal.
+</Typography><Typography>
+help: Shows all commands available.
+</Typography><Typography>
+clear: Wipes the terminal screen. Your filled or submitted forms stay as they are.
+</Typography><Typography>
+register &lt;domain&gt;: Sign up for a domain.</Typography><Typography>
+Example: To register for app, type: &apos;register app&apos;</Typography><Typography>
+
+formsubmit &lt;domain&gt;: submits a form for a domain (you cannot edit the form once submitted)</Typography>
+<Typography>Example: To submit the form for management, type: &apos;formsubmit management&apos;
+</Typography><Typography>
+exit: Closes the terminal.</Typography><Typography>
+
+FORMS</Typography><Typography>
+After registering for a domain via the terminal, fill out the information using the following instructions.
+</Typography><Typography>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+</Typography><Typography>
+Now submit the form using the terminal command. You can register and submit forms for as many domains as you&apos;re interested in.
+</Typography><Typography>
+🎨 Theme & Profile Customization</Typography><Typography>
+Located above the terminal button, the theme button lets you switch between Dark 🌙 and Light ☀️ themes.
+</Typography><Typography>
+Using the profile button you can either edit your profile details or sign out.
+</Typography><Typography>
+📱 Social Media Links</Typography><Typography>
+On the far-left side, there&apos;s a collection of icons representing various ACM social media platforms.
+</Typography><Typography>
+
+
+That concludes our guide! You're now equipped to navigate the ACM Enrollment Website with ease and efficiency. Enjoy your experience! 🚀
+
+                </Typography>
             </Grid>
         </Grid>
     );
--- app/content.ts
+++ app/content.ts
@@ -84,8 +84,91 @@ Our management team at ACM has consistently demonstrated expertise in curating i
 
     },
     extras: {
-        events: `# 📖 Events`,
-        faqs: `# 📖 FAQs`,
-        whois: `# 📖 Whois`,
+        events: `# 📖 Events
+## TINY HACK
+The Tiny Hack is a 10-hour-long hackathon that is designed for individuals who are interested in developing and building innovative projects in a short amount of time. The event is focused on creating small, but impactful solutions to real-world problems using technology. Participants will have the opportunity to collaborate and work with like-minded individuals from diverse backgrounds in order to develop their ideas. 
+
+## CRYPTIC HUNT
+ACM-VIT's Hybrid Cryptic Hunt is an elaborate treasure hunt where our participants will be given questions to solve using skills from the cryptography and cybersecurity domains. Through the Hunt, the participants will get to explore both the world of cybersec and that of VIT, in search of the answers to your problems in the most unexpected places.
+
+## CODEX CRYPTUM
+Codex Cryptum is a specialized workshop organized by ACM-VIT, focusing on deepening participants' understanding of cybersecurity and cryptography. The event offers hands-on sessions and interactions with industry specialists, enabling attendees to delve into advanced topics and gain practical insights. Through this workshop, participants can expand their knowledge base, refine their skills, and engage in meaningful discussions with peers and experts in the field.
+
+## FORKTOBER
+Forktober Fest is an engaging event hosted by ACM-VIT, tailored for participants to contribute to open-source projects. This collaborative initiative encourages participants to actively engage with ACM-VIT's repositories and have the opportunity to explore, modify, and enhance existing projects, while collaborating with like-minded individuals.
+
+## REVERSE CODING
+Reverse Coding is a challenging event conducted by ACM-VIT, designed to challenge and inspire participants in the realm of problem-solving and code deciphering. Unlike traditional coding challenges, this event flips the script. The teams are tasked with reverse engineering and deducing the code based on provided inputs and outputs. It's a test of analytical thinking, coding prowess, and creative problem-solving. 
+
+## HACK STORY
+HackStory is a 24 hour hackathon featuring multiple tracks for diverse interests. Participants tackle real-world challenges, collaborate with peers, and engage with industry mentors, all in a condensed time frame focused on innovation and skill-building.
+`,
+        faqs: `# 📖 FAQs
+<style >
+#ques{
+font-size: 18px;
+
+}
+#ans{
+margin-top: 5px;
+border-top: 1px solid #aaa;
+padding-top: 7px;
+padding-left: 30px;
+font-size: 17px;
+}
+#cont{
+margin-top: 10px;
+background-color: rgba(100, 100, 100, 0.15);  
+border-radius: 5px; 
+padding: 15px;
+}
+</style>
+
+
+<details id= "cont">
+<summary id= "ques"> Do I need prior skills and knowledge to apply for ACM's Organizing Committee?
+</summary>
+<div id= "ans">
+Not at all! Everyone is welcome to apply in any domain.
+All you need is a thirst for knowledge and a drive to grow and develop!
+</div>
+</details>
+
+<details id= "cont">
+<summary id= "ques"> What is the procedure?
+</summary>
+<div id= "ans">
+There are 3 rounds:
+Round 1: Applicants have to fill the form according to their interests,
+ and to the best of their abilities.
+Round 2: Applicants have to face an interview, and accept a task. 
+For Management, applicants have to participate in a Group Discussion.
+Round 3: The final one-on-one interview round!
+Best of Luck!
+</details>
+`,
+        whois: `# 📖 Whois
+<style >
+#mark{
+background-color: rgba(100, 100, 100, 0.15);
+border-radius: 5px;
+padding: 15px;
+margin-top: 10px;
+}</style>
+## Hey Tech enthusiast!!
+
+<div id="mark">ACM-VIT, established in 2009, stands as a highly esteemed technical chapter within VIT, Vellore, committed to rewriting the code of excellence ever since. More than just an entity, we consider ourselves a closely-knit family, united by a passion for innovation and a love for all things computing. 
+</div>
+
+<div style="margin: 15px;">
+ ACM-VIT has been working on projects related to graphic designing, web development, machine learning and app development and has been organizing events and workshop for the same. Apart from this, ACM-VIT also boasts of its own research wing, the only chapter in VIT to have that.
+</div>
+
+<div id= "mark">Whether it's through collaborative coding sessions, peer-to-peer mentoring, or collaborative projects, our club is a space where knowledge is shared freely. From casual meet-ups and social events to team-building activities, we prioritize the importance of having fun and building lasting connections.
+</div>
+
+<div style="margin: 15px;">
+Ready to embark on a tech journey right here on campus? Join ACM, and let's craft coding memories together in college!
+</div>`,
     }
 }
