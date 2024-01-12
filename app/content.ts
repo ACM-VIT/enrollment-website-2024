7b017cec61f9dae12c23b577a9268c3c532df75d
Eshita Jain <eshitajain555@gmail.com>
2024-01-12 19:24:57 +0530

    fix: emojis in content added

--- app/content.ts
+++ app/content.ts
@@ -1,85 +1,85 @@
 export default {
     domain: {
         app: `# 📖 App
-## WHO WE ARE
-Hey App enthusiast!! Welcome to ACM's App domain, where we craft cutting-edge, user-friendly applications for Android and iOS platforms.
-Specializing in Flutter, Kotlin, and Swift, our team crafts event-specific apps designed for engaging user interactions. 
+## WHO WE ARE 👥
+Hey App enthusiast!!📲 Welcome to ACM's App domain, where we craft cutting-edge, user-friendly applications for Android and iOS platforms.📱
+Specializing in Flutter, Kotlin, and Swift, our team crafts event-specific apps designed for engaging user interactions. 🚀
         
         
-## WHAT WE DO
-> Create a dedicated mobile application for "Cryptic Hunt" to provide participants with real-time leaderboard updates, notifications, and other interactive features. Enhance engagement and accessibility for users on the go.
+## WHAT WE DO 🛠️
+> Create a dedicated mobile application for "Cryptic Hunt" to provide participants with real-time leaderboard updates, notifications, and other interactive features. Enhance engagement and accessibility for users on the go.🏆📲
 
-> Incorporate hands-on app development challenges, workshops, and interactive demos within the hackathons held by ACM like HackStory and Tiny Hack.
+> Incorporate hands-on app development challenges, workshops, and interactive demos within the hackathons held by ACM like HackStory and Tiny Hack.💻🛠️
 
-> Collaborate on mobile app projects on ACM-VIT’s open repositories to optimize existing apps or introduce innovative features during our event “Forktober”.
+> Collaborate on mobile app projects on ACM-VIT’s open repositories to optimize existing apps or introduce innovative features during our event “Forktober”.🤝🌐
 `,
         cc: `# 📖 Competitive Coding
-## WHO WE ARE
-Hey CC enthusiast!! Welcome to ACM's Competitive Coding domain, a haven for individuals committed to pushing their coding boundaries. Comprising passionate coders, this domain hosts events providing a platform for students to compete in a challenging yet supportive environment. 
+## WHO WE ARE 👥
+Hey CC enthusiast!!💻 Welcome to ACM's Competitive Coding domain, a haven for individuals committed to pushing their coding boundaries. Comprising passionate coders, this domain hosts events providing a platform for students to compete in a challenging yet supportive environment. 💻🏆
 
 
-## WHAT WE DO
-> Organize Reverse Coding, a one-of-a-kind contest that stands out in the coding community. It spearheads the creation of innovative problem sets, testing participants' coding abilities to the limit. 
+## WHAT WE DO 🛠️
+> Organize Reverse Coding, a one-of-a-kind contest that stands out in the coding community. It spearheads the creation of innovative problem sets, testing participants' coding abilities to the limit.🔄🔍
 
 > Organize coding challenges related to cryptography and puzzle-solving in "Cryptic Hunt".
-Host coding contests and mentorship sessions to support participants in developing innovative projects for "Tiny Hack" and “Hack Story". 
+Host coding contests and mentorship sessions to support participants in developing innovative projects for "Tiny Hack" and “Hack Story".🧩🚀
 
-> Incorporate hands-on coding exercises, workshops and interactive demonstrations into "Codex Cryptum".
-Beyond internal events, Competitive Coding members actively represent VIT in prestigious external competitions like the ACM ICPC, showcasing their skills on a global stage.
+> Incorporate hands-on coding exercises, workshops and interactive demonstrations into "Codex Cryptum".🛠️💡
+Beyond internal events, Competitive Coding members actively represent VIT in prestigious external competitions like the ACM ICPC, showcasing their skills on a global stage.🌐🏅
 `,
         design: `# 📖 Design
-## WHO WE ARE
-Hey Design enthusiasts!! Welcome to ACM's Design domain, your hub for all things creative and visual. Dive deep into the world of design aesthetics, user experience, and innovation. From mastering fundamental design principles to designing prototypes that bring ideas to life, this is your space to experiment and innovate.
+## WHO WE ARE 👥
+Hey Design enthusiasts!!🎨 Welcome to ACM's Design domain, your hub for all things creative and visual. Dive deep into the world of design aesthetics, user experience, and innovation. From mastering fundamental design principles to designing prototypes that bring ideas to life, this is your space to experiment and innovate.✨
 
-## WHAT WE DO
-> Design visually appealing graphics, UI/UX,  themes, and branding materials that capture the essence of "Cryptic Hunt" including a captivating logo, website, and app. 
+## WHAT WE DO 🛠️
+> Design visually appealing graphics, UI/UX,  themes, and branding materials that capture the essence of "Cryptic Hunt" including a captivating logo, website, and app. 🖼️💻
 
-> Create promotional content to generate excitement and engagement for internal events such as "Tiny Hack" , “HackStory” and “Forktober”; while also establishing a cohesive visual identity that reflects the event's theme and objectives.
+> Create promotional content to generate excitement and engagement for internal events such as "Tiny Hack" , “HackStory” and “Forktober”; while also establishing a cohesive visual identity that reflects the event's theme and objectives.🚀🎉
 
-> Craft an aesthetically pleasing and engaging interface for the "Reverse Coding" platform, enhancing the participant experience and facilitating intuitive navigation through the contest challenges.
+> Craft an aesthetically pleasing and engaging interface for the "Reverse Coding" platform, enhancing the participant experience and facilitating intuitive navigation through the contest challenges.🌐👩‍💻
 
 `,
     research: `# 📖 Research
-## WHO WE ARE
-Hey Research enthusiasts!! Welcome to ACM's Research domain, where we focus on research in areas such as cybersecurity, blockchain, and Artificial Intelligence (AI) and Machine Learning (ML). 
-It's not just about geeking out on the latest tech trends; here, we're breaking down the complexities, keeping it real, and making an impact. 
+## WHO WE ARE 👥
+Hey Research enthusiasts!!📚 Welcome to ACM's Research domain, where we focus on research in areas such as cybersecurity, blockchain, and Artificial Intelligence (AI) and Machine Learning (ML). 
+It's not just about geeking out on the latest tech trends; here, we're breaking down the complexities, keeping it real, and making an impact.💡
 
 
 
-## WHAT WE DO
+## WHAT WE DO 🛠️
 
-> Construct intricate and challenging puzzles for "Cryptic Hunt" and "Reverse Coding" by exploring various ciphers, decoders, and puzzle-solving techniques to craft a captivating and intellectually stimulating experience for participants.
+> Construct intricate and challenging puzzles for "Cryptic Hunt" and "Reverse Coding" by exploring various ciphers, decoders, and puzzle-solving techniques to craft a captivating and intellectually stimulating experience for participants.🕵️‍♂️🧩
 
-> Explore emerging technologies and coding trends to curate relevant themes and tracks for Hackathons held by ACM. Promote AI/ML, Cybersecurity, and Blockchain through mentorship, interdisciplinary collaboration, and networking opportunities.
+> Explore emerging technologies and coding trends to curate relevant themes and tracks for Hackathons held by ACM. Promote AI/ML, Cybersecurity, and Blockchain through mentorship, interdisciplinary collaboration, and networking opportunities.🚀🔒
 
-> Conduct insightful workshops like "Codex Cryptum" by researching deeper into innovative applications within the fields of Cryptography, Cybersecurity and Blockchain and provide a hands-on experience for participants.
+> Conduct insightful workshops like "Codex Cryptum" by researching deeper into innovative applications within the fields of Cryptography, Cybersecurity and Blockchain and provide a hands-on experience for participants.🛠️🔍
 
 `,
         web: `# 📖 Web
-## WHO WE ARE
-Hey Web enthusiast!! Welcome to ACM's Web domain, where we explore the full spectrum of web development, from foundational languages to cutting-edge frameworks like React, Next.js, Django, and Flask. 
-Whether you're delving into front-end design, mastering back-end functionalities, or navigating the complexities of web technologies, our community encourages skill development and collaboration. 
+## WHO WE ARE 👥
+Hey Web enthusiast!!🌐 Welcome to ACM's Web domain, where we explore the full spectrum of web development, from foundational languages to cutting-edge frameworks like React, Next.js, Django, and Flask. 
+Whether you're delving into front-end design, mastering back-end functionalities, or navigating the complexities of web technologies, our community encourages skill development and collaboration. 💻👩‍💻
 
-## WHAT WE DO
-> Develop a comprehensive event portal for events like Cryptic Hunt and Hackathons featuring registration forms, event schedule, live updates, and submission guidelines. Ensure a responsive design that caters to participants accessing the portal during the events.
+## WHAT WE DO 🛠️
+> Develop a comprehensive event portal for events like Cryptic Hunt and Hackathons featuring registration forms, event schedule, live updates, and submission guidelines. Ensure a responsive design that caters to participants accessing the portal during the events.📅🖥️
 
-> Collaborate on web development projects within ACM-VIT's open repositories, enhancing existing websites or introducing new features during our event, 'Forktober'.
+> Collaborate on web development projects within ACM-VIT's open repositories, enhancing existing websites or introducing new features during our event, 'Forktober'.🤝🌐
 
-> Develop a platform for hosting the "Reverse Coding" event, ensuring a seamless user experience for participants accessing contest details and submitting solutions.
+> Develop a platform for hosting the "Reverse Coding" event, ensuring a seamless user experience for participants accessing contest details and submitting solutions.💻🔍
 
 `,
         management: `# 📖 Management
-## WHO WE ARE
-Hey Management enthusiast!! Welcome to ACM's Management domain, where solid planning meets hands-on execution, making things happen efficiently.
+## WHO WE ARE 👥
+Hey Management enthusiast!!📊 Welcome to ACM's Management domain, where solid planning meets hands-on execution, making things happen efficiently.📈
 
-Our management team at ACM has consistently demonstrated expertise in curating impactful events that engage and inspire. 
+Our management team at ACM has consistently demonstrated expertise in curating impactful events that engage and inspire. 🚀
 
-## WHAT WE DO
-> Organize our signature event, “Cryptic Hunt”, attracting over 600 enthusiastic participants—a testament to ACM's adept management in crafting engaging experiences.
+## WHAT WE DO 🛠️
+> Organize our signature event, “Cryptic Hunt”, attracting over 600 enthusiastic participants—a testament to ACM's adept management in crafting engaging experiences.🎉📱
 
-> Coordinate all aspects of the 24+ hour hackathons, like HackStory and Tiny Hack including collaborating with sponsors and speakers and ensuring smooth communication and coordination throughout the event.
+> Coordinate all aspects of the 24+ hour hackathons, like HackStory and Tiny Hack including collaborating with sponsors and speakers and ensuring smooth communication and coordination throughout the event.🤝💼
 
-> Host workshops like Codex Cryptum with notable speakers, drawing audiences of over 300 participants.
+> Host workshops like Codex Cryptum with notable speakers, drawing audiences of over 300 participants.🌟📆
 `,
 
     },
