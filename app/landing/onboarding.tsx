c0cf4542dbdfd761aa21ef3d32ff3981121ffbb6
Supratim <supratim7ghose@gmail.com>
2024-01-14 18:42:33 +0530

    onboarding done

--- app/landing/onboarding.tsx
+++ app/landing/onboarding.tsx
@@ -1,33 +1,38 @@
 import "./onboarding.css";
-// import "/AcmLogo.svg";
-// import "../public/introduce-yourself.svg";
 const Onboarding = () => {
   return (
-    <div className="container">
-      <img src="/H2.png" alt="" className="H2" />
-      <img src="/Rectangle 26.png" alt="" className="side_half" />
-      <img src="/Dots.png" alt="" className="top_dots" />
-      <img src="/rect.png" alt="" className="top_rect" />
-      <img src="/Rectangle 25.png" alt="" className="top_half" />
+    <div className="container1">
+      <img src="/H2.png" alt="" className="H21" />
+      <img src="/Rectangle 26.png" alt="" className="side_half1" />
+      <img src="/Dots.png" alt="" className="top_dots1" />
+      <img src="/rect.png" alt="" className="top_rect1" />
+      <img src="/Rectangle 25.png" alt="" className="top_half1" />
 
-      <div className="main">
-        <div className="profile">
+      <div className="main1">
+        <div className="profile1">
           <h1>PROFILE</h1>
         </div>
         <form>
-          <input type="text" name="" placeholder="Contact Number" />
+          <input
+            type="number"
+            maxLength={10}
+            name=""
+            placeholder="Contact Number"
+          />
           <input type="text" name="" placeholder="Name" />
-          <input type="text" name="" placeholder="E-Mail" />
+          <input type="email" name="" placeholder="E-Mail" />
           <input type="text" name="" placeholder="Registration Number" />
-          <button type="submit">SUBMIT</button>
+          <button type="submit" id="submit_button">
+            SUBMIT
+          </button>
         </form>
-    </div>
+      </div>
 
-      <img src="/rect.png" alt="" className="bottom_rect" />
-      <img src="/Dots.png" alt="" className="bottom_dots" />
-      <img src="/Rectangle 27.png" alt="" className="side_half2" />
-      <img src="/Rectangle 24.png" alt="" className="bottom_half" />
-      <img src="/H2 rev.png" alt="" className="H2_rev" />
+      <img src="/rect.png" alt="" className="bottom_rect1" />
+      <img src="/Dots.png" alt="" className="bottom_dots1" />
+      <img src="/Rectangle 27.png" alt="" className="side_half21" />
+      <img src="/Rectangle 24.png" alt="" className="bottom_half1" />
+      <img src="/H2 rev.png" alt="" className="H2_rev1" />
     </div>
   );
 };
