dbc90e25c2cdfff7956c916acb000aaeb9f91985
Supratim Ghose <supratim7ghose@gmail.com>
2024-01-31 22:03:40 +0530

    formsubmit done

--- app/components/Terminal.tsx
+++ app/components/Terminal.tsx
@@ -4,29 +4,27 @@ import { Container, color, fontSize, width } from "@mui/system";
 import { Box } from "@mui/material";
 import { formSubmit, registerDomain } from "@/app/actions/terminal";
 import { Domain } from "@prisma/client";
-import PagesContext from "@/lib/PagesContext";
 import { pages as pagesGenerator } from "../pages/pages";
 import { ReactTerminal } from "react-terminal";
 
 const terminalText = (
-    <span
-      style={{
-        color: "#0DBC79",
-      }}
-    >
-      [user@acm-mainframe]~$
-    </span>
-  );
-  
-  const welcomeMessage = (
-    <span>
-      Welcome to the ACM Mainframe Terminal! Type <strong>'help'</strong> for a
-      list of commands
-      <br />
-      <br />
-    </span>
-  );
+  <span
+    style={{
+      color: "#0DBC79",
+    }}
+  >
+    [user@acm-mainframe]~$
+  </span>
+);
 
+const welcomeMessage = (
+  <span>
+    Welcome to the ACM Mainframe Terminal! Type <strong>'help'</strong> for a
+    list of commands
+    <br />
+    <br />
+  </span>
+);
 
 function Terminal({
   setShowTerminal,
@@ -46,8 +44,8 @@ function Terminal({
         ┝ help - displays this message <br />
         ┝ clear - clears the terminal <br />
         ┝ register &lt;domain&gt; - registers a domain <br />
-        ┝ formsubmit &lt;domain&gt; - submits a form for a domain, <br />└ exit - exits
-        the terminal
+        ┝ formsubmit &lt;domain&gt; - submits a form for a domain, <br />└ exit
+        - exits the terminal
       </span>
     ),
     exit: () => {
@@ -60,11 +58,28 @@ function Terminal({
     register: async (domain: string) => {
       if (!domain) {
         return <span style={{ color: "#FF443E" }}>Domain not specified</span>;
-        }
-        if (!(domain in Domain)) {
-            return <span style={{ color: "#FF443E" }}>Domain not found</span>;
-        }
+      }
+      if (!(domain in Domain)) {
+        return <span style={{ color: "#FF443E" }}>Domain not found</span>;
+      }
       const response = await registerDomain(domain as Domain);
+      if (response.registrations) {
+        setPages(pagesGenerator(response.registrations));
+      }
+      return response.console.type === "error" ? (
+        <span style={{ color: "#FF443E" }}>{response.console.message}</span>
+      ) : (
+        response.console.message
+      );
+    },
+    formsubmit: async (domain: string) => {
+      if (!domain) {
+        return <span style={{ color: "#FF443E" }}>Domain not specified</span>;
+      }
+      if (!(domain in Domain)) {
+        return <span style={{ color: "#FF443E" }}>Domain not found</span>;
+      }
+      const response = await formSubmit(domain as Domain);
       if (response.registrations) {
         setPages(pagesGenerator(response.registrations));
       }
@@ -76,68 +91,65 @@ function Terminal({
     },
   };
 
-  return     <>
-  <Box
-    sx={{
-      height: "15%",
-      marginTop: "1px",
-      // marginBottom: "0px",
-      textDecoration: "underline #0078d4",
-      textUnderlineOffset: "5px",
-      paddingLeft: "28px",
-      paddingTop: "3px",
-      fontFamily: "consolas regular",
-      color: "inherit",
-    }}
-  >
-    <span
-      style={{
-        fontSize: "75%",
-        fontFamily: "inherit",
-        textTransform: "uppercase",
-        cursor: "context-menu",
-      }}
-    >
-      Terminal
-    </span>
-  </Box>
-  <Container
-    sx={{
-      height: "85%",
-              marginLeft: "0px",
-    paddingRight: "0px",
+  return (
+    <>
+      <Box
+        sx={{
+          height: "15%",
+          marginTop: "1px",
+          // marginBottom: "0px",
+          textDecoration: "underline #0078d4",
+          textUnderlineOffset: "5px",
+          paddingLeft: "28px",
+          paddingTop: "3px",
+          fontFamily: "consolas regular",
+          color: "inherit",
+        }}
+      >
+        <span
+          style={{
+            fontSize: "75%",
+            fontFamily: "inherit",
+            textTransform: "uppercase",
+            cursor: "context-menu",
           }}
-          maxWidth={false}
-  >
-          <ReactTerminal
-              style={{
-                  fontSize: "12",
-              }}
-      commands={commands}
-      themes={{
-        dark1: {
-          themeBGColor: "#1e1e1e",
-          themeToolbarColor: "#424242",
-          themeColor: "#fff",
-          themePromptColor: "#42A5F5",
-        },
-        light1: {
-          themeBGColor: "#fafafa",
-          themeToolbarColor: "#424242",
-          themeColor: "#151515",
-          themePromptColor: "#42A5F5",
-        },
-      }}
-      showControlBar={false}
-      welcomeMessage={welcomeMessage}
-      theme={dark ? "dark1" : "light1"}
-      prompt={terminalText}
-      errorMessage={
-        <span style={{ color: "#FF443E" }}>Command not found</span>
-      }
-    />
-  </Container>
-</>;
+        >
+          Terminal
+        </span>
+      </Box>
+      <Container
+        sx={{
+          height: "85%",
+        }}
+        maxWidth={false}
+      >
+        <ReactTerminal
+          commands={commands}
+          themes={{
+            dark1: {
+              themeBGColor: "#1e1e1e",
+              themeToolbarColor: "#424242",
+              themeColor: "#fff",
+              themePromptColor: "#42A5F5",
+            },
+            light1: {
+              themeBGColor: "#fafafa",
+              themeToolbarColor: "#424242",
+              themeColor: "#151515",
+              themePromptColor: "#42A5F5",
+            },
+          }}
+          showControlBar={false}
+          welcomeMessage={welcomeMessage}
+          theme={dark ? "dark1" : "light1"}
+          prompt={terminalText}
+          errorMessage={
+            <span style={{ color: "#FF443E" }}>Command not found</span>
+          }
+        />
+      </Container>
+    </>
+  );
 }
 
 export default Terminal;
